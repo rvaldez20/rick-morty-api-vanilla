4ebaa275510d5f32196ac86e12319b3c1c52fb2b
rvaldez20 <30907228+rvaldez20@users.noreply.github.com>
2020-07-14 21:51:40 -0500

    Se muestra en DOM la data de la API

--- static/js/api.js
+++ static/js/api.js
@@ -1,9 +1,11 @@
 export default class API {
 
    // metodo para obtener un character
-   getCharacter(id){
-   	fetch(`https://rickandmortyapi.com/api/character/${id}`)
-			.then((response) => response.json())
-			.then((data) => console.log(data))
+   async getCharacter(id){
+	const response = await fetch(`https://rickandmortyapi.com/api/character/${id}`)
+	const data = await response.json()
+	return data
+		// .then((response) => response.json())
+		// .then((data) => data)
 	}
 }
--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,49 @@
 import API from './api.js';
-
 const api = new API();
-console.log(api.getCharacter(4));
+
+const characterContainer = document.querySelector('#characterContainer');
+
+// const characterTemaplate = `
+//    <article class="character">
+//    <div class="character-grid">
+//       <h2>Rick Sánchez</h2>
+//       <img src="" alt="">
+//    </div>
+//    </article>
+// `
+
+class character {
+   constructor({ name, image }){
+      this.name = name
+      this.image = image
+      this.render()
+   }
+
+   build(){
+      return `
+      <article class="character">
+      <div class="character-grid">
+         <h2>${this.name}</h2>
+         <img src="${this.image}" alt="">
+      </div>
+      </article>
+   `
+   }
+
+   render() {
+      characterContainer.innerHTML = this.build()
+   }
+}
+
+
+async function initApp(initCharacterId) {
+   const characterData = await api.getCharacter(initCharacterId);
+   const rick = new character(characterData)
+   
+   // console.log(characterData);
+}
+
+initApp(1);
+
+// rick.render();
+// console.log(api.getCharacter(4));
